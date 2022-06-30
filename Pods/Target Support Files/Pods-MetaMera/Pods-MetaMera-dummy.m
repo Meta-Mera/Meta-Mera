389087cb143d2a93650f19b4129362406c1eff8f
jim <nakamura.jim.k@gmail.com>
2022-06-30 16:27:34 +0900

    update

---
+++ Pods/Target Support Files/Pods-MetaMera/Pods-MetaMera-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MetaMera : NSObject
+@end
+@implementation PodsDummy_Pods_MetaMera
+@end
---
+++ Pods/Target Support Files/Pods-MetaMeraTests/Pods-MetaMeraTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MetaMeraTests : NSObject
+@end
+@implementation PodsDummy_Pods_MetaMeraTests
+@end
