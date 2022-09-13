e142d10f645877c22c4a184d57202a1a6b974136
jim <nakamura.jim.k@gmail.com>
2022-09-13 17:18:24 +0900

    1.1.4 プロフィール編集画面を少し進めた

--- MetaMera/Views/Profile/ChangeProfile/ChangeProfileViewController.swift
+++ MetaMera/Views/Profile/ChangeProfile/ChangeProfileViewController.swift
@@ -8,12 +8,17 @@
 import Foundation
 import UIKit
 import Firebase
+import Nuke
+import IQKeyboardManagerSwift
 
 class ChangeProfileViewController: UIViewController {
     
     
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var userProfileImageView: UIImageView!
+    @IBOutlet weak var uploadButton: UIButton!
     
     var loginUser: User!
     
@@ -24,14 +29,27 @@ class ChangeProfileViewController: UIViewController {
         super.viewDidLoad()
         
         configView()
+//        IQKeyboardManager.shared.enable = true
     }
     
     func configView(){
-        userNameLabel.text = loginUser.userName
+        userNameLabel.text = "ユーザネームの変更"
+        userNameTextField.text = loginUser.userName
+        userProfileImageView.layer.cornerRadius = userProfileImageView.bounds.width / 2
+        if let url = URL(string: loginUser.profileImage){
+            Nuke.loadImage(with: url, into: userProfileImageView)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     @IBAction func pushBackButton(_ sender: Any) {
-        print("push back image")
+        print("push back button.")
         self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func pushUploadProfile(_ sender: Any) {
+//        backButton.isEnabled = false
     }
 }
