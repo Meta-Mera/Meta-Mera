5f765a88da29f9560796ce164891ec7dd8bae24a
yuta3003 <y.nakazawa3003+github@gmail.com>
2022-11-07 11:31:07 +0900

    create account view

---
+++ MetaMera/Views/SignUp/SettingProfile/SettingProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingProfileViewController.swift
+//  MetaMera
+//
+//  Created by admin on 2022/11/04.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class SettingProfileViewController : UIViewController {
+    
+    @IBOutlet weak var userIconButton: UIButton!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configView()
+        bind()
+    }
+    
+    private func configView() {
+        
+    }
+    
+    private func bind() {
+        
+    }
+    
+    @IBAction func nextButtonAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func userIconButtonAction(_ sender: Any) {
+        a
+    }
+    
+}
