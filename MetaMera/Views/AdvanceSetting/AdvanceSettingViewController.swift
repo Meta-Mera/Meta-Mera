abe92ff3fd5a5308e05f1f1f7f39cd690863e6e1
yuta3003 <y.nakazawa3003+github@gmail.com>
2022-11-25 14:20:50 +0900

    :+1: create AdvanceSetting table view 動作確認できていないので月曜日に動作確認する。

--- MetaMera/Views/AdvanceSetting/AdvanceSettingViewController.swift
+++ MetaMera/Views/AdvanceSetting/AdvanceSettingViewController.swift
@@ -7,23 +7,60 @@
 
 import UIKit
 
-class AdvanceSettingViewController: UIViewController {
+public struct SettingItem {
+    var title: String
+    var description: String
+}
 
+final class SettingModel {
+    let items: [SettingItem] = [
+        .init(title: "メールアドレスの設定", description: "現在登録されているメールアドレスの確認と変更をすることができます。"),
+        .init(title: "パスワードの変更", description: "現在登録しているパスワードを変更することができます。"),
+        .init(title: "通知設定", description: "通知の種類を設定することができます。"),
+        .init(title: "お問い合わせ", description: "何かお困りのことがある場合やバグの修正依頼などお気軽にお問合わせください。"),
+        .init(title: "退会", description: "Meta-Meraのアカウントを削除し退会する手続きを行います。"),
+    ]
+}
+
+class AdvanceSettingViewController: UIViewController {
+    
+    private lazy var items: [SettingItem] = {
+        let model = SettingModel()
+        let items = model.items
+        return items
+    }()
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.delegate = self
+            tableView.register(.init(nibName: "AdvanceSettingTableViewCell", bundle: .main), forCellReuseIdentifier: "AdvanceSettingTableViewCell")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AdvanceSettingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AdvanceSettingTableViewCell") as? AdvanceSettingTableViewCell else {
+            return UITableViewCell()
+        }
+        let item = items[indexPath.row]
+        cell.bind(item)
+        return cell
+    }
+}
 
+extension AdvanceSettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected", indexPath.row)
+    }
 }
