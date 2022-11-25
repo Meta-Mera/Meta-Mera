b54fa7518824a95afde24a9ffc156391b8a20e93
yuta3003 <y.nakazawa3003+github@gmail.com>
2022-11-25 11:26:23 +0900

    :memo: create Basic Screen. 詳細設定画面の基本画面設計を作成

---
+++ MetaMera/Views/AdvanceSetting/AdvanceSettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AdvanceSettingViewController.swift
+//  MetaMera
+//
+//  Created by y.nakazawa on 2022/11/25.
+//
+
+import UIKit
+
+class AdvanceSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
