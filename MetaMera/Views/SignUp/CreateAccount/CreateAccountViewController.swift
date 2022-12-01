c2764b372776f54f867b96cac90cab524c4f2267
yuta3003 <y.nakazawa3003+github@gmail.com>
2022-12-01 13:58:34 +0900

    :+1: CreateAccount画面の修正完了

--- MetaMera/Views/SignUp/CreateAccount/CreateAccountViewController.swift
+++ MetaMera/Views/SignUp/CreateAccount/CreateAccountViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var photoMarkButton: UIButton!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
