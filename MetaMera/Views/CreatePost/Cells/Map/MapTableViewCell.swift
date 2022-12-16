2116e40700622860ce89ebedbb42f021dd69ee99
jim <nakamura.jim.k@gmail.com>
2022-12-16 13:41:48 +0900

    mapに円を書く処理を追加

--- MetaMera/Views/CreatePost/Cells/Map/MapTableViewCell.swift
+++ MetaMera/Views/CreatePost/Cells/Map/MapTableViewCell.swift
@@ -7,15 +7,31 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapTableViewCell: UITableViewCell {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var postButton: UIButton!
     
+    var locationManager = LocationManager()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func setUpCircle(){
+        locationManager.startLocation()
+        mapView.showsUserLocation = true
+        
+        // 円を描画する(半径500m).
+        let myCircle: MKCircle = MKCircle(center: CLLocationCoordinate2D(latitude: mapView.userLocation.coordinate.latitude,longitude: mapView.userLocation.coordinate.longitude), radius: CLLocationDistance(500))
+//
+//        center = CLLocationCoordinate2D(latitude: mapView.userLocation.coordinate.latitude,longitude: mapView.userLocation.coordinate.longitude)
+        
+        // mapViewにcircleを追加.
+        mapView.addOverlay(myCircle)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
