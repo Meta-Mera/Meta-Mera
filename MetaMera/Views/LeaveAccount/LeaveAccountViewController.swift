e6a84b1aa1b10b787dce019d299bc9c5e9724c66
yuta3003 <y.nakazawa3003+github@gmail.com>
2022-12-19 10:36:09 +0900

    :up: 退会画面作成に伴うファイル作成

---
+++ MetaMera/Views/LeaveAccount/LeaveAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LeaveAccountViewController.swift
+//  MetaMera
+//
+//  Created by y.nakazawa on 2022/12/19.
+//
+
+import UIKit
+
+class LeaveAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
