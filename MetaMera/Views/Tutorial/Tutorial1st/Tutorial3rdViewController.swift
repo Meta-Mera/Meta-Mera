09c1392b72a02b368871cf862a3c746c35992bd3
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-01-31 10:46:54 +0900

    create Tutorial 3rd

---
+++ MetaMera/Views/Tutorial/Tutorial1st/Tutorial3rdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Tutorial3rdViewController.swift
+//  MetaMera
+//
+//  Created by y.nakazawa on 2023/01/31.
+//
+
+import UIKit
+
+class Tutorial3rdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
