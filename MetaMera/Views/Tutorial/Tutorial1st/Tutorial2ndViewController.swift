912b16cd9390f30591534cf17e9356c87772b5bd
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-01-31 10:22:24 +0900

    create Tutorial 2

---
+++ MetaMera/Views/Tutorial/Tutorial1st/Tutorial2ndViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Tutorial2ndViewController.swift
+//  MetaMera
+//
+//  Created by y.nakazawa on 2023/01/31.
+//
+
+import UIKit
+
+class Tutorial2ndViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
